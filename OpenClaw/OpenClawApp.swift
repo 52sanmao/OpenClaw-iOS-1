8b3a70517619d41b52c27af48ca2f070f2d0568d
Parham Majd <parham@parhammajd.co.uk>
2026-03-27 17:54:20 +0000

    Initial Commit

---
+++ OpenClaw/OpenClawApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenClawApp.swift
+//  OpenClaw
+//
+//  Created by Parham on 27/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenClawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
